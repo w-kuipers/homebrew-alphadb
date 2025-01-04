a1388de363938c96301eb18cf4f8cb9f02e33f8f
Wibo Kuipers <wibokuip@gmail.com>
2025-01-04 15:28:51 +0100

    Update latest version

--- alphadb.rb
+++ alphadb.rb
@@ -3,8 +3,8 @@ class Alphadb < Formula
   homepage "https://alphadb.w-kuipers.com/"
   license "GPL-3.0-or-later"
 
-  url "https://github.com/w-kuipers/alphadb/releases/download/v1.0.0-beta.28/alphadb-cli_v1.0.0-beta.28_Darwin-x86_64.tar.gz"
-  sha256 "bbc186c3e74f95e627114d043928273bd0f421f549f89c19faf4185558818fa9"
+  url "https://github.com/w-kuipers/alphadb/releases/download/v1.0.0-beta.30/alphadb-cli_v1.0.0-beta.30_Darwin-x86_64.tar.gz"
+  sha256 "d6001b50940d08f44c49121505a4750825ad29086327c6d4912f3c89a75c3f8d"
 
   def install
     libexec.install Dir["*"]
