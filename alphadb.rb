da1a6da7518b655f39d5732c5fd0be41119a80b8
Wibo Kuipers <wibokuip@gmail.com>
2026-04-06 13:23:15 +0200

    Bump version

--- alphadb.rb
+++ alphadb.rb
@@ -3,22 +3,22 @@ class Alphadb < Formula
   homepage "https://alphadb.w-kuipers.com/"
   license "GPL-3.0-or-later"
 
-  version "1.0.0-beta.39"
+  version "1.0.0-beta.40"
 
   on_macos do
     on_arm do
       url "https://github.com/w-kuipers/alphadb/releases/download/v#{version}/alphadb-cli_v#{version}_Darwin-aarch64.tar.gz"
-      sha256 "1d50f34339a42b4b3f9a3085f48177657a39e8e5f6fbc7a1e73acfa04e2c95df"
+      sha256 "sha256:8b290963cdac43a8e0054e2e07d311ad2e7e0b044d69550ccd00c78b5c4a4390"
     end
     on_intel do
       url "https://github.com/w-kuipers/alphadb/releases/download/v#{version}/alphadb-cli_v#{version}_Darwin-x86_64.tar.gz"
-      sha256 "d909d393e245ce5d33474abdaf886ababe7394489e0f7a81d0646be13cf8bdb5"
+      sha256 "sha256:535fff37141f2f396b7e80efe422975246128f74567e8582bdb4d44e7b33e2c3"
     end
   end
 
   on_linux do
     url "https://github.com/w-kuipers/alphadb/releases/download/v#{version}/alphadb-cli_v#{version}_Linux-x86_64.tar.gz"
-    sha256 "c4f5ef6c03118a1e0a562e55c00ef853c80f236a3e8483ff5e12117ad2233f2f"
+    sha256 "sha256:2f835c59c1502390c621ed9298bd73e4d776f69cef6d5fe4ae62eaa65a51dde2"
   end
 
   def install
